d33abb5e9f42c0e65130390026a3186e3ba89f78
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-04-26 20:02:37 -0500

    Spinner Selector

--- billeteka/src/main/java/com/cool4code/billeteka/HomeActivity.java
+++ billeteka/src/main/java/com/cool4code/billeteka/HomeActivity.java
@@ -1,15 +1,40 @@
 package com.cool4code.billeteka;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class HomeActivity extends Activity {
+    private Spinner ourcolorspinner;
+    private String selection;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ourcolorspinner = (Spinner) findViewById(R.id.home_spinner);
+        listenerMethod();
+    }
+
+    private void listenerMethod() {
+        // TODO Auto-generated method stub
+        ourcolorspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id){
+                selection = (String) parentView.getItemAtPosition(position);
+                Log.d("id", selection);
+                //Toast.makeText(context, "You seleceted " + selection + ".", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView){
+
+            }});
+
     }
 }
