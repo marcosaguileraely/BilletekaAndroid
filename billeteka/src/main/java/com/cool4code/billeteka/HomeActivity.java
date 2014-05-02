11aecedcf91915122b699973e0cca02f0820c367
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-05-02 02:12:00 -0500

    Query on Cloud and Save data into sqlite

--- billeteka/src/main/java/com/cool4code/billeteka/HomeActivity.java
+++ billeteka/src/main/java/com/cool4code/billeteka/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,8 +35,8 @@ public class HomeActivity extends Activity implements OnClickListener{
     TextView paises;
     ProgressDialog mProgressDialog;
     ProgressDialog barProgressDialog;
-
     private SQLiteDatabase mydb;
+    int queryLimit= 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,26 +52,37 @@ public class HomeActivity extends Activity implements OnClickListener{
         //Parse.initialize(this, "XdWuoWJKZ4rL1hv2lGfgB9IP4fy5po65xayBwwNW", "Ky1GlA3n4jiEnTTLreBjDzo94J5tLshdnZ9sDR1e");
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
-        // If you would like all objects to be private by default, remove this line.
         defaultACL.setPublicReadAccess(true);
         ParseACL.setDefaultACL(defaultACL, true);
+        new RemoteDataTask().execute();
     }
-    @Override
-    public void onClick(View v) {
-        File db =new File("/data/data/com.cool4code.billeteka/databases/billsdb");
-        String objectId, dia, mes, ano, denominacion, serie, descripcion, tiempo;
-        String f1_4, f5_7, f8_10, p1_4, p5_7, p8_10, bernardom13, createdAt, updatedAt;
-        int size;
-        if(db.exists()){
+    // RemoteDataTask AsyncTask
+    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(HomeActivity.this);
+            mProgressDialog.setTitle("Billeteka");
+            mProgressDialog.setMessage("Estamos descargando datos. Solo unos segundos...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            File db =new File("/data/data/com.cool4code.billeteka/databases/billsdb");
+            String objectId, dia, mes, ano, denominacion, serie, descripcion, tiempo;
+            String f1_4, f5_7, f8_10, p1_4, p5_7, p8_10, bernardom13, createdAt, updatedAt;
+            int size;
+            if(db.exists()){
             Log.d("->", "Existe db");
-            mydb = this.openOrCreateDatabase("billsdb", MODE_PRIVATE, null);
+            Log.d("->", "¡NOTHING TO-DO HERE!");
+            //mydb = this.openOrCreateDatabase("billsdb", MODE_PRIVATE, null);
+            /*SQLiteDatabase mydb = getBaseContext().openOrCreateDatabase("billsdb", SQLiteDatabase.OPEN_READWRITE, null);
             mydb.execSQL("CREATE TABLE IF NOT EXISTS "+ "banknote" + "(objectId VARCHAR,dia VARCHAR,mes VARCHAR,ano VARCHAR,denominacion VARCHAR,serie VARCHAR,descripcion VARCHAR,tiempo VARCHAR,f1_4 VARCHAR,f5_7 VARCHAR,f8_10 VARCHAR,p1_4 VARCHAR,p5_7 VARCHAR,p8_10 VARCHAR,bernardom13 VARCHAR,createdAt VARCHAR,updatedAt VARCHAR);");
             mydb.execSQL("DELETE FROM banknote");
             try {
-                final ProgressDialog ringProgressDialog = ProgressDialog.show(HomeActivity.this, "Billeteka message", "Downloading data from cloud...", true);
-                ringProgressDialog.setCancelable(true);
                 ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("billeteca2014");
-                query.setLimit(1000); // limit to at most 10 results
+                query.setLimit(queryLimit); // limit to at most 10 results
                 ob = query.find();
                 size= ob.size();
                 Log.d("Accion"," total: "+size);
@@ -94,55 +106,65 @@ public class HomeActivity extends Activity implements OnClickListener{
                     bernardom13 = bills.getString("bernardom13");
                     createdAt = bills.getString("createdAt");
                     updatedAt = bills.getString("updatedAt");
-                    Log.d("Accion","-> "+size+" - "+objectId);
+                    Log.d("Accion","-> "+size+" - "+objectId+" - "+size--);
                     mydb.execSQL("INSERT INTO banknote"+"(objectId, dia, mes, ano, denominacion, serie, descripcion, tiempo, f1_4, f5_7, f8_10, p1_4, p5_7, p8_10, bernardom13, createdAt, updatedAt)"+
                                  "VALUES ('"+objectId+"','"+dia+"','"+mes+"','"+ano+"','"+denominacion+"','"+serie+"','"+descripcion+"','"+tiempo+"','"+f1_4+"','"+f5_7+"','"+f8_10+"','"+p1_4+"','"+p5_7+"','"+p8_10+"','"+bernardom13+"','"+createdAt+"','"+updatedAt+"');");
                 }
             } catch (ParseException e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
-            }
-        }else{
-            Log.d("->", "No Existe db");
-            mydb = this.openOrCreateDatabase("billsdb", MODE_PRIVATE, null);
-            Log.d("->", "billsdb Creada");
-            mydb.execSQL("CREATE TABLE IF NOT EXISTS "+ "banknote" + "(objectId VARCHAR,dia VARCHAR,mes VARCHAR,ano VARCHAR,denominacion VARCHAR,serie VARCHAR,descripcion VARCHAR,tiempo VARCHAR,f1_4 VARCHAR,f5_7 VARCHAR,f8_10 VARCHAR,p1_4 VARCHAR,p5_7 VARCHAR,p8_10 VARCHAR,bernardom13 VARCHAR,createdAt VARCHAR,updatedAt VARCHAR);");
-            Log.d("->", "tabla banknote creada");
-            try {
-                Log.d("MyApp", "Iniciando busqueda! ->");
-                ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("billeteca2014");
-                query.setLimit(1000); // limit to at most 10 results
-                ob = query.find();
-                size= ob.size();
-                Log.d("Accion"," total: "+size);
-                for (ParseObject bills : ob) {
-                    objectId= bills.getObjectId();
-                    dia = bills.getString("dia");
-                    mes = bills.getString("mes");
-                    ano = bills.getString("ano");
-                    denominacion = bills.getString("denominacion");
-                    serie = bills.getString("serie");
-                    descripcion = bills.getString("descripcion");
-                    tiempo = bills.getString("tiempo");
-                    f1_4 = bills.getString("f1_4");
-                    f5_7 = bills.getString("f5_7");
-                    f8_10 = bills.getString("f8_10");
-                    p1_4 = bills.getString("p1_4");
-                    p5_7 = bills.getString("p5_7");
-                    p8_10 = bills.getString("p8_10");
-                    bernardom13 = bills.getString("bernardom13");
-                    createdAt = bills.getString("createdAt");
-                    updatedAt = bills.getString("updatedAt");
-                    Log.d("Accion","-> "+size+" - "+objectId);
-                    mydb.execSQL("INSERT INTO banknote"+"(objectId, dia, mes, ano, denominacion, serie, descripcion, tiempo, f1_4, f5_7, f8_10, p1_4, p5_7, p8_10, bernardom13, createdAt, updatedAt)"+
-                            "VALUES ('"+objectId+"','"+dia+"','"+mes+"','"+ano+"','"+denominacion+"','"+serie+"','"+descripcion+"','"+tiempo+"','"+f1_4+"','"+f5_7+"','"+f8_10+"','"+p1_4+"','"+p5_7+"','"+p8_10+"','"+bernardom13+"','"+createdAt+"','"+updatedAt+"');");
+            }*/
+            }else{
+                Log.d("->", "No Existe db");
+                SQLiteDatabase mydb = getBaseContext().openOrCreateDatabase("billsdb", SQLiteDatabase.OPEN_READWRITE, null);
+                //this.openOrCreateDatabase("billsdb", MODE_PRIVATE, null);
+                Log.d("->", "billsdb Creada");
+                mydb.execSQL("CREATE TABLE IF NOT EXISTS "+ "banknote" + "(objectId VARCHAR,dia VARCHAR,mes VARCHAR,ano VARCHAR,denominacion VARCHAR,serie VARCHAR,descripcion VARCHAR,tiempo VARCHAR,f1_4 VARCHAR,f5_7 VARCHAR,f8_10 VARCHAR,p1_4 VARCHAR,p5_7 VARCHAR,p8_10 VARCHAR,bernardom13 VARCHAR,createdAt VARCHAR,updatedAt VARCHAR);");
+                Log.d("->", "tabla banknote creada");
+                try {
+                    Log.d("MyApp", "Iniciando busqueda! ->");
+                    ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("billeteca2014");
+                    query.setLimit(queryLimit); // limit to at most 10 results
+                    ob = query.find();
+                    size= ob.size();
+                    Log.d("Accion"," total: "+size);
+                    for (ParseObject bills : ob) {
+                        objectId= bills.getObjectId();
+                        dia = bills.getString("dia");
+                        mes = bills.getString("mes");
+                        ano = bills.getString("ano");
+                        denominacion = bills.getString("denominacion");
+                        serie = bills.getString("serie");
+                        descripcion = bills.getString("descripcion");
+                        tiempo = bills.getString("tiempo");
+                        f1_4 = bills.getString("f1_4");
+                        f5_7 = bills.getString("f5_7");
+                        f8_10 = bills.getString("f8_10");
+                        p1_4 = bills.getString("p1_4");
+                        p5_7 = bills.getString("p5_7");
+                        p8_10 = bills.getString("p8_10");
+                        bernardom13 = bills.getString("bernardom13");
+                        createdAt = bills.getString("createdAt");
+                        updatedAt = bills.getString("updatedAt");
+                        Log.d("Accion","-> "+size+" - "+objectId+" - "+size--);
+                        mydb.execSQL("INSERT INTO banknote"+"(objectId, dia, mes, ano, denominacion, serie, descripcion, tiempo, f1_4, f5_7, f8_10, p1_4, p5_7, p8_10, bernardom13, createdAt, updatedAt)"+
+                                "VALUES ('"+objectId+"','"+dia+"','"+mes+"','"+ano+"','"+denominacion+"','"+serie+"','"+descripcion+"','"+tiempo+"','"+f1_4+"','"+f5_7+"','"+f8_10+"','"+p1_4+"','"+p5_7+"','"+p8_10+"','"+bernardom13+"','"+createdAt+"','"+updatedAt+"');");
+                    }
+                } catch (ParseException e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
                 }
-            } catch (ParseException e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
             }
+            return null;
         }
+        @Override
+        protected void onPostExecute(Void result) {
+            mProgressDialog.dismiss();
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
         /*Intent goToListActivity= new Intent(HomeActivity.this, ListActivity.class);
         goToListActivity.putExtra("CALORIE", calorie);
         goToListActivity.putExtra("PROTEIN", protein);
